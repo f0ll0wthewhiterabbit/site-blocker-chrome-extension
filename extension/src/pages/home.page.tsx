bd6b64cb0c423c07fd4f45ea238a9395b1966263
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2023-12-02 21:07:28 +0300

    Installed tailwindcss

--- extension/src/pages/home.page.tsx
+++ extension/src/pages/home.page.tsx
@@ -3,5 +3,5 @@ import { useSessionQuery } from '@/entities/session';
 export function HomePage() {
   const { data: session } = useSessionQuery();
 
-  return <div>Home Page {session?.email}</div>;
+  return <div className="text-rose-500">Home Page {session?.email}</div>;
 }
